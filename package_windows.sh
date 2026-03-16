1441514caa700bf5e201a469cdc777e4ca698c55
Charles Bennington <c.bennington852@gmail.com>
2026-03-16 16:58:10 -0700

    feat : packaging changes

--- package_windows.sh
+++ package_windows.sh
@@ -3,3 +3,6 @@ source myenv/Scripts/activate
 pip install -r requirments.txt
 pip install -e .
 pyinstaller windows.spec -y
+IF_PATH="C:\Program Files (x86)\solicus\InstallForge\bin\ifbuildx86.exe"
+PROJECT_PATH="C:\Projects\MyApp\Setup.ifp"
+$IF_PATH $PROJECT_PATH
